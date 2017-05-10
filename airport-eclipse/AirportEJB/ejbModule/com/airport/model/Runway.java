fff9958ca4d07c6858f504854880864c5ec4b9f6
Timon <timon.boehm@outlook.de>
2017-05-10 14:59:52 +0200

    Add basic runway

---
+++ airport-eclipse/AirportEJB/ejbModule/com/airport/model/Runway.java
@@ -0,0 +1,38 @@
+package com.airport.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+
+@NamedQuery(name="runway.findAll", query="select a from Runway a order by a.runwayNo")
+
+@Entity
+public class Runway {
+	
+	@Id
+	@GeneratedValue
+	private int id;
+
+	private int maxRunways = 4; 	
+	private int runwayNo;
+	private boolean isFree = True;
+
+	public int getrunwayNo() {
+		return runwayNo;
+	}
+	
+	public void setRunwayNo(int rwNo) {
+		if(rwNo <= maxRunways && rwNo > 0){
+			this.runwayNo = rwNo;	
+		}		
+	}
+
+	public int getMaxRunways() {
+		return maxRunways;
+	}
+
+	public boolean getStatus() {
+		return isFree;
+	}
+}
