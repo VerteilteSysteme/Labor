e8af0e2c9c2de1b596ab17bf095a36edc75b05dc
Hallenyoyo <fabian-schilling@gmx.net>
2017-05-31 14:51:42 +0200

    Added ParkingPosition funktion in Bean

--- airport-eclipse/AirportEJB/ejbModule/com/airport/model/ParkingPosition.java
+++ airport-eclipse/AirportEJB/ejbModule/com/airport/model/ParkingPosition.java
@@ -7,9 +7,9 @@ import javax.persistence.NamedQuery;
 import javax.persistence.NamedQueries;
 
 @NamedQueries({
-	@NamedQuery(name="parkingPosition.findAll", query="select a from ParkingPosition a order by a.parkingPos"),
-	@NamedQuery(name="parkingPosition.findPos", query="select a from ParkingPosition a where a.parkingPos = :pos"),
-	@NamedQuery(name="parkingPosition.findFree", query="select a from ParkingPosition a where a.isFree = true order by a.parkingPos"),
+	@NamedQuery(name="parkingPosition.findAll", query="select a from ParkingPosition a order by a.parkingPositionNo"),
+	@NamedQuery(name="parkingPosition.findPos", query="select a from ParkingPosition a where a.parkingPositionNo = :pos"),
+	@NamedQuery(name="parkingPosition.findFree", query="select a from ParkingPosition a where a.isFree = true order by a.parkingPositionNo"),
 })
 
 @Entity
@@ -18,17 +18,17 @@ public class ParkingPosition {
 	@Id
 	@GeneratedValue
 	private int id;	
-	private int parkingPos;
+	private int parkingPositionNo;
 	private boolean isFree = true;
 
 	public ParkingPosition(){}
 
 	public ParkingPosition(int rwNo) {
-		this.parkingPos = rwNo;	
+		this.parkingPositionNo = rwNo;	
 	}
 	
-	public int getparkingPosition() {
-		return parkingPos;
+	public int getparkingPositionNo() {
+		return parkingPositionNo;
 	}
 
 
